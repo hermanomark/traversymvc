41e2673ad56b08dfde6f8de177be0837a9875531
hermanomark <hermano.mark.mh@gmail.com>
2018-11-22 17:41:45 +0800

    done chapter 4, 4 vids

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,6 +1,15 @@
 <?php 
+    // Load Config
+    require_once 'config/config.php';
+
     // Load Libraries
-    require_once 'libraries/core.php';
-    require_once 'libraries/controller.php';
-    require_once 'libraries/database.php';
+    // require_once 'libraries/core.php';
+    // require_once 'libraries/controller.php';
+    // require_once 'libraries/database.php';
+
+    // To minimize requiring libraries 1by1 we could use Autoload
+    // Autoload Core Libraries
+    spl_autoload_register(function($className) {
+        require_once 'libraries/' . $className . '.php';
+    });
 ?>
--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,15 +1,23 @@
 <?php
-    class Pages {
+    class Pages extends Controller {
         public function __construct() {
             
         }
 
         public function index() {
-
+            $data = array(
+                'title' => 'Welcome'
+            );
+            
+            $this->view('pages/index', $data);
         }
-        
-        public function about($id) {
-            echo $id;   
+
+        public function about() {
+            $data = array(
+                'title' => 'About Us'
+            );
+
+            $this->view('pages/about', $data);
         }
     }
 ?>
